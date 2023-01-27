be8c604ceb1c85dc8f585e3d19cbc1b6b77b0d1e
Arthur40480 <arthurgibert@free.fr>
2023-01-27 16:41:54 +0100

    Update Phase1 extract book

---
+++ Phase1.py
@@ -0,0 +1,79 @@
+# Importation des librairies
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+# Phase 1
+url_fiction_book = "http://books.toscrape.com/catalogue/william-shakespeares-star-wars-verily-a-new-hope-william-shakespeares-star-wars-4_871/index.html"
+
+# Création d'une liste pour les en-têtes:
+heading = [
+    "product_page_url",
+    "universal_product_code",
+    "title",
+    "price_including_tax",
+    "price_excluding_tax",
+    "number_available",
+    "product_description",
+    "category",
+    "review_rating",
+    "image_url"
+]
+
+# Fonction pour récupérer les données d'un livre.
+def extract_datas_book(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    datas_book = soup.find_all("td")
+    product_page_url = url
+    universal_product_code = datas_book[0].text
+    title = soup.find("h1").text
+    price_including_tax = datas_book[3].text
+    price_excluding_tax = datas_book[2].text
+    number_available = datas_book[5].text
+    product_description = soup.select_one("article > p").text
+    category = soup.find("ul", class_="breadcrumb").find_all("a")[2].text
+    review_rating = soup.find("p", class_="star-rating").get("class")[1] + " in five"
+    base_url = "http://books.toscrape.com/"
+    image_url = soup.find("div", class_="item active").find("img").get("src")
+    image = (base_url + image_url).replace("../../", "")
+
+    # On créer un dictionnaire pour chaque livre:
+    fiction_book = {
+        "product_page_url": product_page_url,
+        "universal_product_code": universal_product_code,
+        "title": title,
+        "price_including_tax": price_including_tax,
+        "price_excluding_tax": price_excluding_tax,
+        "number_available": number_available,
+        "product_description": product_description,
+        "category": category,
+        "review_rating": review_rating,
+        "image_url": image
+    }
+
+    print(fiction_book)
+
+    #On appelle la fonction create_csv_file:
+    create_csv_file(fiction_book, heading)
+
+# Fonction pour créer un fichier csv, et y enregistrer les données du livre.
+def create_csv_file(fiction_book, heading):
+    with open('Book_data.csv', 'w')as fichier_csv:
+        writer = csv.writer(fichier_csv, delimiter=",")
+        writer.writerow(heading)
+        writer.writerow([
+            fiction_book["product_page_url"],
+            fiction_book["universal_product_code"],
+            fiction_book["title"],
+            fiction_book["price_including_tax"],
+            fiction_book["price_excluding_tax"],
+            fiction_book["number_available"],
+            fiction_book["product_description"],
+            fiction_book["category"],
+            fiction_book["review_rating"],
+            fiction_book["image_url"]
+        ])
+
+extract_datas_book(url_fiction_book)
+
