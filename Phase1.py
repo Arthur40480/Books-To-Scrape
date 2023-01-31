0a5dc67b7af0365111c8672789d6e8efcbadd6d8
Arthur40480 <arthurgibert@free.fr>
2023-01-31 19:15:58 +0100

    Data path update and image download

--- Phase1.py
+++ Phase1.py
@@ -1,4 +1,5 @@
 # Importation des librairies
+import os
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -19,6 +20,9 @@ heading = [
     "review_rating",
     "image_url"
 ]
+# Création du dossier book_data si celui-ci n'est pas déjà créer:
+if not os.path.isdir('product\\book_data'):
+    os.system('mkdir product\\book_data')
 
 # Fonction pour récupérer les données d'un livre.
 def extract_datas_book(url):
@@ -51,15 +55,15 @@ def extract_datas_book(url):
         "review_rating": review_rating,
         "image_url": image
     }
-
     print(fiction_book)
 
-    #On appelle la fonction create_csv_file:
+    # On appelle les fonctions create_csv_file et extract_img_product:
     create_csv_file(fiction_book, heading)
+    extract_img_product(category, image)
 
 # Fonction pour créer un fichier csv, et y enregistrer les données du livre.
 def create_csv_file(fiction_book, heading):
-    with open('Book_data.csv', 'w')as fichier_csv:
+    with open('product\\book_data\\' + fiction_book["category"] + '.csv', 'w')as fichier_csv:
         writer = csv.writer(fichier_csv, delimiter=",")
         writer.writerow(heading)
         writer.writerow([
@@ -75,5 +79,13 @@ def create_csv_file(fiction_book, heading):
             fiction_book["image_url"]
         ])
 
+# Fonction pour télécharger les images à partir de leurs URLS:
+def extract_img_product(category, url):
+    f = open('product\\book_data\\' + category + '.jpg', 'wb')
+    reponse_img = requests.get(url)
+    f.write(reponse_img.content)
+    f.close()
+
+# On apelle la fonction extract_datas_book pour lancer le script:
 extract_datas_book(url_fiction_book)
 
