f1ea7f91cd4872ba1d716f3805dbee3a27f9e4ff
Arthur40480 <arthurgibert@free.fr>
2023-02-01 17:24:54 +0100

    Naming files update

--- Phase1.py
+++ Phase1.py
@@ -52,12 +52,13 @@ def extract_datas_book(url):
         "review_rating": review_rating,
         "image_url": image
     }
-    print(fiction_book)
+    title_folder = title.replace(":", "").replace("/", " ").replace('"', '').replace(" ", "").replace(
+        'Ã©', 'é').replace(",", "").replace(".", "").replace("&", "").replace("*", "").replace("?", "").replace("#", "")
 
     # On appelle les fonctions create_csv_file et extract_img_product:
     create_folder()
-    create_csv_file(fiction_book, heading)
-    extract_img_product(category, image)
+    create_csv_file(fiction_book, title_folder, heading)
+    extract_img_product(title_folder, image)
 
 # Fonction pour créer un dossier book_data si celui-ci n'est pas déjà créer:
 def create_folder():
@@ -65,8 +66,8 @@ def create_folder():
         os.system('mkdir product\\book_data')
 
 # Fonction pour créer un fichier csv, et y enregistrer les données du livre.
-def create_csv_file(fiction_book, heading):
-    with open('product\\book_data\\' + fiction_book["category"] + '.csv', 'w')as fichier_csv:
+def create_csv_file(fiction_book, title, heading):
+    with open('product\\book_data\\' + title + '.csv', 'w')as fichier_csv:
         writer = csv.writer(fichier_csv, delimiter=",")
         writer.writerow(heading)
         writer.writerow([
@@ -83,8 +84,8 @@ def create_csv_file(fiction_book, heading):
         ])
 
 # Fonction pour télécharger les images à partir de leurs URLS:
-def extract_img_product(category, url):
-    f = open('product\\book_data\\' + category + '.jpg', 'wb')
+def extract_img_product(title, url):
+    f = open('product\\book_data\\' + title + '.jpg', 'wb')
     reponse_img = requests.get(url)
     f.write(reponse_img.content)
     f.close()
